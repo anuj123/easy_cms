b9f19ac0ca6b5715d69c91038d1525123ec68de2
Anuj <anuj.thapliyal@gmail.com>
2012-09-03 20:45:58 -0700

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyCms::Application.config.secret_token = '35781d32f9062028fc57df2a7183df37e04ad609227ce6abf9a2ad65f31ef08b5d0d451cfe9aaa90c9403e58acd123cdb738a06100d39440c8e7904613f84058'
